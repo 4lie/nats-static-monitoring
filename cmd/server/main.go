e987a8defd825920473898933df8d81dc51da7d3
saman <Saman.Hoseini@snapp.cab>
2020-09-16 00:10:50 +0430

    fix: remove and move extra cmd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,24 @@ package server
 
 import (
 	"github.com/4lie/nats-static-monitoring/config"
+	"github.com/4lie/nats-static-monitoring/scheduler"
+	"github.com/olivere/elastic"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 func main(cfg config.Config) {
+	client, err := elastic.NewClient(elastic.SetURL(cfg.Elasticsearch.Servers...))
+	if err != nil {
+		logrus.Panicf("failed to create elasticsearch client: %s", err.Error())
+	}
+
+	elasticWriter := scheduler.NewElasticWriter(client)
+	monitoringScheduler := scheduler.New(elasticWriter)
+
+	if err := monitoringScheduler.Start(cfg.Scheduler.CronPattern, cfg.MonitorServers); err != nil {
+		logrus.Fatalf("Failed to start scheduler: %s", err.Error())
+	}
 }
 
 // Register Server command.
