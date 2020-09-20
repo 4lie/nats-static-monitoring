1219a3b6d2542a95e83f68c010ca6fe210c70871
Rashad Ansari <rashad.ansari1996@gmail.com>
2020-09-20 23:36:00 +0430

    fix: Add fore merge for nats and nats streaming indices

--- scheduler/elastic_writer.go
+++ scheduler/elastic_writer.go
@@ -22,12 +22,18 @@ func NewElasticWriter(elasticClient *elastic.Client) *ElasticWriter {
 
 func (e ElasticWriter) Write(response map[string]*monitor.Response) {
 	for _, value := range response {
+		ctx := context.Background()
+
 		index := fmt.Sprintf("%s-%s", value.Type, value.Index)
 
-		_, err := e.ElasticClient.Index().Index(index).Type("doc").Id(value.Key).BodyJson(value.Body).
-			Refresh("false").Do(context.Background())
+		_, err := e.ElasticClient.Index().Index(index).Type("doc").Id(value.Key).BodyJson(value.Body).Do(ctx)
 		if err != nil {
 			logrus.Errorf("unable to write data to elastic index %s with key %s: %s", index, value.Key, err.Error())
+		}
+
+		_, err = e.ElasticClient.Forcemerge(index).Do(ctx)
+		if err != nil {
+			logrus.Errorf("unable to fore merge data for elastic index %s: %s", index, err.Error())
 		}
 	}
 }
